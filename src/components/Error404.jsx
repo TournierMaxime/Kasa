cb17f05411fcd713a7d0d39d5015c3d039c2b9ad
Maxime Tournier <mtournier@devsdefrance.com>
2023-10-05 09:51:02 +0200

    Title + responsive

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,16 +1,21 @@
+import { Fragment } from "react"
 import { Link } from "react-router-dom"
+import Title from "../components/Title"
 
 const Error404 = () => {
   return (
-    <div className="error404">
-      <h1 className="error404__title">404</h1>
-      <h4 className="error404__message">
-        Oups! La page que vous demandez n'existe pas.
-      </h4>
-      <Link className="error404__redirectLink" to={"/"}>
-        Retourner sur la page d'accueil
-      </Link>
-    </div>
+    <Fragment>
+      <Title title={"Erreur 404"} />
+      <div className="error404">
+        <h1 className="error404__title">404</h1>
+        <h4 className="error404__message">
+          Oups! La page que vous demandez n'existe pas.
+        </h4>
+        <Link className="error404__redirectLink" to={"/"}>
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </Fragment>
   )
 }
 
